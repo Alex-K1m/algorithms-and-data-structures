704d4931f2f3bc3fb7a7cacbaeb1df567607d8c2
Alex Kim <45559664+Alex-K1m@users.noreply.github.com>
2021-08-16 16:53:32 +0500

    feat: add more methods to hash table

--- src/data-structures/hash-table/HashTable.js
+++ src/data-structures/hash-table/HashTable.js
@@ -1,10 +1,8 @@
 import LinkedList from '../linked-list/LinkedList.js';
 
 export default class HashTable {
-  /** @arg {[string, *][]} entries */
-  constructor(entries = []) {
+  constructor() {
     this._buckets = Array.from({ length: 10 }, () => new LinkedList());
-    this._keys = entries.map(([key]) => key);
   }
 
   /** @arg {string} key */
@@ -19,18 +17,38 @@ export default class HashTable {
    * @arg {string} key
    * @arg {*} value
    */
-  set() {}
+  set(key, value) {
+    const bucket = this._buckets[this._hash(key)];
+    const node = bucket.find((pair) => pair[0] === key);
+
+    if (node) node.value[1] = value;
+    else bucket.prepend([key, value]);
+
+    return this;
+  }
 
   /** @arg {string} key */
-  get() {}
+  get(key) {
+    const bucket = this._buckets[this._hash(key)];
+    const node = bucket.find((pair) => pair[0] === key);
+    return node?.value[1];
+  }
 
   /** @arg {string} key */
-  has() {}
+  has(key) {
+    const bucket = this._buckets[this._hash(key)];
+    const node = bucket.find((pair) => pair[0] === key);
+    return !!node;
+  }
 
   /** @arg {string} key */
-  delete() {}
+  delete(key) {
+    const bucket = this._buckets[this._hash(key)];
+    bucket.delete((pair) => pair[0] === key);
+    return this;
+  }
 
-  // getKeys, values, entries?
+  // TODO get key/value/entries
 
   [Symbol.toPrimitive]() {
     const items = this._buckets
--- src/data-structures/hash-table/HashTable.test.js
+++ src/data-structures/hash-table/HashTable.test.js
@@ -2,6 +2,25 @@ import LinkedList from '../linked-list/LinkedList.js';
 import HashTable from './HashTable.js';
 
 describe('HashTable', () => {
+  /** @type {[string, *][]} */
+  const pairs = [
+    ['a', 'str'],
+    ['b', 42],
+    ['k', true],
+    ['l', null],
+  ];
+
+  /** @type {HashTable} */
+  let filledHashTable;
+
+  beforeEach(() => {
+    filledHashTable = new HashTable()
+      .set(...pairs[0])
+      .set(...pairs[1])
+      .set(...pairs[2])
+      .set(...pairs[3]);
+  });
+
   it('generates proper hashes for keys', () => {
     const hashTable = new HashTable();
 
@@ -27,19 +46,64 @@ describe('HashTable', () => {
     ].join('\n');
 
     const emptyHashTable = new HashTable();
-    const hashTable = new HashTable(entries);
+    const hashTable = new HashTable();
     hashTable._buckets = entries.map((entry) => new LinkedList([entry]));
 
     expect(String(emptyHashTable)).toBe('');
     expect(String(hashTable)).toBe(expected);
   });
 
-  it.todo('creates a hash table from array of key-value pairs');
-  it.todo('adds new pairs');
-  it.todo('overwrites a value for existing key');
-  it.todo('returns a value for a given key');
-  it.todo('checks it contains a given key');
-  it.todo('deletes pairs');
-  it.todo('returns a list of all keys/values/entries?');
-  it.todo('collisions');
+  describe('adds new pairs and handles collisions', () => {
+    test.each(pairs)('case %#', (key, value) => {
+      const hashTable = filledHashTable;
+      const index = hashTable._hash(key);
+      const node = hashTable._buckets[index].find(
+        ([savedKey]) => savedKey === key,
+      );
+
+      expect(node).not.toBeNull();
+      expect(node.value).toEqual([key, value]);
+    });
+  });
+
+  it('overwrites a value for existing key', () => {
+    const hashTable = filledHashTable;
+    const index = hashTable._hash('k');
+    const { head } = hashTable._buckets[index];
+
+    expect(head.value).toEqual(['k', true]);
+
+    hashTable.set('k', 13);
+    const headAfterSet = hashTable._buckets[index].head;
+
+    expect(headAfterSet).toBe(head);
+    expect(head.value).toEqual(['k', 13]);
+  });
+
+  it('returns a value for a given key', () => {
+    const hashTable = filledHashTable;
+
+    expect(hashTable.get('a')).toBe('str');
+    expect(hashTable.get('b')).toBe(42);
+    expect(hashTable.get('c')).toBeUndefined();
+    expect(hashTable.get('l')).toBeNull();
+  });
+
+  it('checks if contains a given key', () => {
+    const hashTable = filledHashTable;
+
+    expect(hashTable.has('a')).toBe(true);
+    expect(hashTable.has('b')).toBe(true);
+    expect(hashTable.has('c')).toBe(false);
+  });
+
+  it('deletes pairs', () => {
+    const hashTable = filledHashTable;
+    hashTable.delete('a').delete('l');
+
+    expect(hashTable.has('a')).toBe(false);
+    expect(hashTable.has('b')).toBe(true);
+    expect(hashTable.has('k')).toBe(true);
+    expect(hashTable.has('l')).toBe(false);
+  });
 });
