7470ed6e0590c348016e1da2f0a89fbc884311c3
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2021-12-03 14:42:41 +0500

    refactor: linked list to ts

--- src/data-structures/linked-list/LinkedList.test.ts
+++ src/data-structures/linked-list/LinkedList.test.ts
@@ -1,27 +1,27 @@
-import LinkedList from './LinkedList.js';
-import LinkedListNode from './LinkedListNode.js';
+import LinkedList from './LinkedList';
+import LinkedListNode from './LinkedListNode';
 
 describe('LinkedList', () => {
   it('creates a list from array', () => {
-    const items = [1, 'str', true];
+    const items = [1, 2, 3];
     const list = new LinkedList(items);
-    const singleValueList = new LinkedList([1]);
+    const singleValueList = new LinkedList(['single']);
 
     expect(list.isEmpty()).toBe(false);
-    expect(list.head.value).toBe(1);
-    expect(list.last.value).toBe(true);
+    expect(list.head?.value).toBe(1);
+    expect(list.last?.value).toBe(3);
     expect(String(list)).toBe(String(items));
-    expect(list.head.next.value).toBe('str');
-    expect(list.last.next).toBeNull();
+    expect(list.head?.next?.value).toBe(2);
+    expect(list.last?.next).toBeNull();
 
     expect(singleValueList.head).toBe(singleValueList.last);
-    expect(singleValueList.head.next).toBeNull();
+    expect(singleValueList.head?.next).toBeNull();
   });
 
   it('creates a list from node', () => {
     const node = new LinkedList([1, 2, 3]).head;
-    const list = new LinkedList(node);
-    const singleNode = new LinkedListNode(1);
+    const list = new LinkedList(node as LinkedListNode<number>);
+    const singleNode = new LinkedListNode('single');
     const singleValueList = new LinkedList(singleNode);
 
     expect(list.isEmpty()).toBe(false);
@@ -43,34 +43,34 @@ describe('LinkedList', () => {
 
   it('prepends a value', () => {
     const list = new LinkedList([1, 2]).prepend(0);
-    const singleValueList = new LinkedList().prepend(0);
+    const singleValueList = new LinkedList<number>().prepend(0);
 
-    expect(list.head.value).toBe(0);
-    expect(list.head.next.value).toBe(1);
+    expect(list.head?.value).toBe(0);
+    expect(list.head?.next?.value).toBe(1);
     expect(String(list)).toBe('0,1,2');
 
-    expect(singleValueList.head.value).toBe(0);
-    expect(singleValueList.head.next).toBeNull();
+    expect(singleValueList.head?.value).toBe(0);
+    expect(singleValueList.head?.next).toBeNull();
     expect(singleValueList.head).toBe(singleValueList.last);
     expect(String(singleValueList)).toBe('0');
   });
 
   it('appends a value', () => {
     const list = new LinkedList([1, 2]).append(3);
-    const singleValueList = new LinkedList().append(0);
+    const singleValueList = new LinkedList<number>().append(0);
 
-    expect(list.last.value).toBe(3);
-    expect(list.last.next).toBeNull();
+    expect(list.last?.value).toBe(3);
+    expect(list.last?.next).toBeNull();
     expect(String(list)).toBe('1,2,3');
 
-    expect(singleValueList.head.value).toBe(0);
-    expect(singleValueList.last.next).toBeNull();
+    expect(singleValueList.head?.value).toBe(0);
+    expect(singleValueList.last?.next).toBeNull();
     expect(singleValueList.head).toBe(singleValueList.last);
     expect(String(singleValueList)).toBe('0');
   });
 
   it('is iterable', () => {
-    const values = [1, false, null, 'str', { key: 42 }];
+    const values = [1, 2, 3, 4];
     const list = new LinkedList(values);
 
     expect(Array.from(list)).toEqual(values);
@@ -81,19 +81,19 @@ describe('LinkedList', () => {
     const actual = list.find(2);
 
     expect(actual).toBeInstanceOf(LinkedListNode);
-    expect(actual.value).toBe(2);
-    expect(actual.next.value).toBe(3);
+    expect(actual?.value).toBe(2);
+    expect(actual?.next?.value).toBe(3);
 
     expect(list.find(4)).toBeNull();
 
-    expect(list.find((val) => typeof val === 'number').value).toBe(1);
+    expect(list.find((value) => value % 2 === 0)?.value).toBe(2);
   });
 
   it('deletes head', () => {
     const list = new LinkedList([1, 2]).deleteHead();
 
-    expect(list.head.value).toBe(2);
-    expect(list.head.next).toBeNull();
+    expect(list.head?.value).toBe(2);
+    expect(list.head?.next).toBeNull();
     expect(list.head).toBe(list.last);
 
     list.deleteHead();
@@ -112,13 +112,13 @@ describe('LinkedList', () => {
   it('deletes last', () => {
     const list = new LinkedList([1, 2, 3]).deleteLast();
 
-    expect(list.last.value).toBe(2);
-    expect(list.last.next).toBeNull();
+    expect(list.last?.value).toBe(2);
+    expect(list.last?.next).toBeNull();
     expect(String(list)).toBe('1,2');
 
     list.deleteLast();
 
-    expect(list.last.value).toBe(1);
+    expect(list.last?.value).toBe(1);
     expect(list.head).toBe(list.last);
 
     list.deleteLast();
@@ -138,20 +138,20 @@ describe('LinkedList', () => {
     const list = new LinkedList([1, 2, 3, 2, 4]).delete(2);
 
     expect(String(list)).toBe('1,3,4');
-    expect(list.head.value).toBe(1);
-    expect(list.head.next.value).toBe(3);
-    expect(list.last.value).toBe(4);
+    expect(list.head?.value).toBe(1);
+    expect(list.head?.next?.value).toBe(3);
+    expect(list.last?.value).toBe(4);
 
     list.delete(1);
 
-    expect(list.head.value).toBe(3);
-    expect(list.head.next.value).toBe(4);
+    expect(list.head?.value).toBe(3);
+    expect(list.head?.next?.value).toBe(4);
     expect(String(list)).toBe('3,4');
 
     list.delete((val) => val === 4);
 
-    expect(list.last.value).toBe(3);
-    expect(list.last.next).toBeNull();
+    expect(list.last?.value).toBe(3);
+    expect(list.last?.next).toBeNull();
     expect(String(list)).toBe('3');
 
     list.delete(5);
--- src/data-structures/linked-list/LinkedList.ts
+++ src/data-structures/linked-list/LinkedList.ts
@@ -1,12 +1,15 @@
-import LinkedListNode from './LinkedListNode.js';
+import LinkedListNode from './LinkedListNode';
 
-export default class LinkedList {
-  /** @arg {Array | LinkedListNode} [list] */
-  constructor(list) {
+export default class LinkedList<T> implements Iterable<T> {
+  head: LinkedListNode<T> | null;
+
+  last: typeof this.head;
+
+  constructor(list?: T[] | LinkedListNode<T>) {
     if (list instanceof LinkedListNode) {
       this.head = list;
-      this.last = (function iterate(node = list) {
-        return node.next === null ? node : iterate(node.next);
+      this.last = (function iterate(node = list): LinkedListNode<T> {
+        return node.next?.hasNext() ? iterate(node.next) : node;
       })();
       return;
     }
@@ -22,89 +25,92 @@ export default class LinkedList {
       return;
     }
 
-    /** @type {LinkedListNode} */
     this.head = null;
-    /** @type {LinkedListNode} */
     this.last = null;
   }
 
-  isEmpty() {
+  isEmpty(): boolean {
     return this.head === null;
   }
 
-  /** @arg {*} value */
-  prepend(value) {
+  prepend(value: T): this {
     const node = new LinkedListNode(value, this.head);
+
     if (this.isEmpty()) this.last = node;
     this.head = node;
+
     return this;
   }
 
-  /** @arg {*} value */
-  append(value) {
+  append(value: T): this {
     const node = new LinkedListNode(value);
+
     if (this.isEmpty()) this.head = node;
-    else this.last.next = node;
+    else this.last!.next = node;
     this.last = node;
+
     return this;
   }
 
-  /**
-   * @arg {*} arg
-   * @return {LinkedListNode}
-   */
-  find(arg) {
-    const iterate = (node = this.head) => {
+  find(value: T): LinkedListNode<T> | null;
+  find(callback: (value: T) => boolean): LinkedListNode<T> | null;
+  find(arg: T | ((value: T) => boolean)): LinkedListNode<T> | null {
+    const iterate = (node = this.head): LinkedListNode<T> | null => {
       if (!node) return null;
 
       const equals =
-        typeof arg === 'function' ? arg(node.value) : node.value === arg;
+        arg instanceof Function ? arg(node.value) : node.value === arg;
       return equals ? node : iterate(node.next);
     };
 
     return iterate();
   }
 
-  deleteHead() {
+  deleteHead(): this {
     if (this.isEmpty()) return this;
 
-    if (this.head.next === null) {
+    if (this.head!.hasNext()) this.head = this.head!.next;
+    else {
       this.head = null;
       this.last = null;
-    } else this.head = this.head.next;
+    }
 
     return this;
   }
 
-  deleteLast() {
+  deleteLast(): this {
     if (this.isEmpty()) return this;
 
-    if (this.head.next === null) {
+    if (!this.head!.hasNext()) {
       this.head = null;
       this.last = null;
       return this;
     }
 
     const iterate = (node = this.head) => {
-      if (node.next === this.last) {
+      if (node!.next === this.last) {
         this.last = node;
-        this.last.next = null;
+        this.last!.next = null;
         return;
       }
-      iterate(node.next);
+      iterate(node!.next);
     };
     iterate();
 
     return this;
   }
 
-  /** @arg {*} arg */
-  delete(arg) {
-    const iterate = (node = this.head, prevNode = new LinkedListNode()) => {
+  delete(value: T): this;
+  delete(callback: (value: T) => boolean): this;
+  delete(arg: T | ((value: T) => boolean)): this {
+    const iterate = (
+      node = this.head,
+      prevNode = new LinkedListNode<unknown>(null) as LinkedListNode<T>,
+    ) => {
       if (node === null) return;
 
       const equals =
-        typeof arg === 'function' ? arg(node.value) : node.value === arg;
+        arg instanceof Function ? arg(node.value) : node.value === arg;
 
       if (equals) {
         // eslint-disable-next-line no-param-reassign
@@ -126,24 +132,18 @@ export default class LinkedList {
     return this;
   }
 
-  [Symbol.toPrimitive]() {
-    /**
-     * @arg {LinkedListNode} node
-     * @arg {Array} acc
-     * @return {Array}
-     */
-    const iterate = (node, acc = []) =>
+  [Symbol.toPrimitive](): string {
+    const iterate = (node = this.head, acc: T[] = []): T[] =>
       node ? iterate(node.next, [...acc, node.value]) : acc;
-    return iterate(this.head).join(',');
+    return iterate().join(',');
   }
 
-  *[Symbol.iterator]() {
-    const self = this;
-    function* iterate(node = self.head) {
-      if (!node) return;
+  *[Symbol.iterator](): Generator<T> {
+    function* iterate(node: LinkedListNode<T> | null): Generator<T> {
+      if (node === null) return;
       yield node.value;
       yield* iterate(node.next);
     }
-    yield* iterate();
+    yield* iterate(this.head);
   }
 }
