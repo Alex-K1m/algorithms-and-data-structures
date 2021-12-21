f42518e8e0fbf7bf3141bd2df12bbdd7889a569f
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2021-12-21 11:30:14 +0500

    refactor: convert deque to ts

--- src/data-structures/deque/Deque.test.ts
+++ src/data-structures/deque/Deque.test.ts
@@ -1,4 +1,4 @@
-import Deque from './Deque.js';
+import Deque from './Deque';
 
 describe('Deque', () => {
   it('creates an empty deque', () => {
@@ -9,41 +9,41 @@ describe('Deque', () => {
   });
 
   it('adds a value to the front', () => {
-    const deque = new Deque().addFront(1).addFront(2).addFront(3);
+    const deque = new Deque<number>().addFront(3).addFront(1, 2);
 
     expect(deque.isEmpty()).toBe(false);
-    expect(String(deque)).toBe('3,2,1');
+    expect(String(deque)).toBe('1,2,3');
   });
 
   it('adds a value to the back', () => {
-    const deque = new Deque().addBack(1).addBack(2).addBack(3);
+    const deque = new Deque<number>().addBack(1).addBack(2, 3);
 
     expect(deque.isEmpty()).toBe(false);
     expect(String(deque)).toBe('1,2,3');
   });
 
   it('removes a value from the front', () => {
-    const deque = new Deque().addFront(1).addFront(2).addFront(3);
+    const deque = new Deque<number>().addFront(1, 2, 3);
 
-    expect(deque.removeFront()).toBe(3);
-    expect(deque.removeFront()).toBe(2);
     expect(deque.removeFront()).toBe(1);
+    expect(deque.removeFront()).toBe(2);
+    expect(deque.removeFront()).toBe(3);
     expect(deque.isEmpty()).toBe(true);
     expect(deque.removeFront()).toBeUndefined();
   });
 
   it('removes a value from the back', () => {
-    const deque = new Deque().addFront(1).addFront(2).addFront(3);
+    const deque = new Deque<number>().addFront(1, 2, 3);
 
-    expect(deque.removeBack()).toBe(1);
-    expect(deque.removeBack()).toBe(2);
     expect(deque.removeBack()).toBe(3);
+    expect(deque.removeBack()).toBe(2);
+    expect(deque.removeBack()).toBe(1);
     expect(deque.isEmpty()).toBe(true);
     expect(deque.removeBack()).toBeUndefined();
   });
 
   it('peeks front', () => {
-    const deque = new Deque();
+    const deque = new Deque<number>();
 
     expect(deque.peekFront()).toBeUndefined();
     expect(deque.addFront(1).peekFront()).toBe(1);
@@ -51,7 +51,7 @@ describe('Deque', () => {
   });
 
   it('peeks back', () => {
-    const deque = new Deque();
+    const deque = new Deque<number>();
 
     expect(deque.peekBack()).toBeUndefined();
     expect(deque.addBack(1).peekBack()).toBe(1);
@@ -59,6 +59,6 @@ describe('Deque', () => {
   });
 
   it('is iterable', () => {
-    expect([...new Deque().addFront(1).addBack(2)]).toEqual([1, 2]);
+    expect([...new Deque<number>().addFront(1).addBack(2)]).toEqual([1, 2]);
   });
 });
