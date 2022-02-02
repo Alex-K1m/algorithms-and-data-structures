6211b32ab07c6d033bf22ae06dbc3fa39292931a
Alex Kim <45559664+alex-kim-dev@users.noreply.github.com>
2022-02-02 14:44:57 +0500

    test: refactor sorting test

--- src/algorithms/sorting/Sorter.test.ts
+++ src/algorithms/sorting/Sorter.test.ts
@@ -9,10 +9,7 @@ const compareNumbers: CompareFn<number> = (a, b) => {
 const compareStringLengths: CompareFn<string> = (
   { length: a },
   { length: b },
-) => {
-  if (a === b) return 0;
-  return a > b ? 1 : -1;
-};
+) => compareNumbers(a, b);
 
 const shuffled = [3, 5, 4, -1, -4, 2, 0, 1, -2, -5, -3];
 const sorted = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
@@ -20,30 +17,20 @@ const reversed = sorted.slice().reverse();
 const shuffledStrings = ['dd', 'aa', 'q', 'a', 'bbbb', 'ccc'];
 const sortedStrings = ['q', 'a', 'dd', 'aa', 'ccc', 'bbbb'];
 
-test('bubble sort works correctly & is stable', () => {
-  const sorter = new Sorter(compareNumbers);
-  const strLengthsSorter = new Sorter(compareStringLengths);
+const methods = ['bubble', 'selection'] as const;
 
-  expect(sorter.bubble([])).toEqual([]);
-  expect(sorter.bubble([1])).toEqual([1]);
-  expect(sorter.bubble([1, 1, 1])).toEqual([1, 1, 1]);
-  expect(sorter.bubble(sorted)).toEqual(sorted);
-  expect(sorter.bubble(reversed)).toEqual(sorted);
-  expect(sorter.bubble(shuffled)).toEqual(sorted);
-  expect(sorter.reverse().bubble(shuffled)).toEqual(reversed);
-  expect(strLengthsSorter.bubble(shuffledStrings)).toEqual(sortedStrings);
-});
+describe('Sorting algorithms work correctly and are stable', () => {
+  test.each(methods.map((item) => [item]))('%s sort', (method) => {
+    const sorter = new Sorter(compareNumbers);
+    const strLengthsSorter = new Sorter(compareStringLengths);
 
-test('selection sort works correctly & is stable', () => {
-  const sorter = new Sorter(compareNumbers);
-  const strLengthsSorter = new Sorter(compareStringLengths);
-
-  expect(sorter.selection([])).toEqual([]);
-  expect(sorter.selection([1])).toEqual([1]);
-  expect(sorter.selection([1, 1, 1])).toEqual([1, 1, 1]);
-  expect(sorter.selection(sorted)).toEqual(sorted);
-  expect(sorter.selection(reversed)).toEqual(sorted);
-  expect(sorter.selection(shuffled)).toEqual(sorted);
-  expect(sorter.reverse().selection(shuffled)).toEqual(reversed);
-  expect(strLengthsSorter.selection(shuffledStrings)).toEqual(sortedStrings);
+    expect(sorter[method]([])).toEqual([]);
+    expect(sorter[method]([1])).toEqual([1]);
+    expect(sorter[method]([1, 1, 1])).toEqual([1, 1, 1]);
+    expect(sorter[method](sorted)).toEqual(sorted);
+    expect(sorter[method](reversed)).toEqual(sorted);
+    expect(sorter[method](shuffled)).toEqual(sorted);
+    expect(sorter.reverse()[method](shuffled)).toEqual(reversed);
+    expect(strLengthsSorter[method](shuffledStrings)).toEqual(sortedStrings);
+  });
 });
